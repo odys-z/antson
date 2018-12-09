e730e5343206e289d9e343bc21e06acaa54d5153
odysz <odysseusj@163.com>
2018-12-09 19:59:06 +0800

    Gson has limitations: https://github.com/google/gson/blob/master/GsonDesignDocument.md take over Gson is difficult because Java is erasure: https://stackoverflow.com/questions/6373800/use-reflection-to-create-a-generic-parameterized-class-in-java

---
+++ antson/src/main/java/io/odysz/antson/Ant.java
@@ -0,0 +1,119 @@
+package io.odysz.antson;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ant {
+
+	/*
+	public static <T extends Ason> String json (T[] t) {
+		// FIXME Performance problem: type() and json() should return streams.
+		// Why we can't purely depend on stream to build string?
+		// https://stackoverflow.com/questions/26158082/how-to-convert-a-tree-structure-in-a-stream-of-nodes-in-java
+		/*
+		String type = Stream.of("{type: \"[",
+						t.length > 0 ? t[0].type() : Ason.class.getName(),
+						"]\", data: [").collect(Collectors.joining(""));
+		StringBuilder b = new StringBuilder(type);
+		* /
+		StringBuilder b = new StringBuilder();
+		b.append("{type: [");
+
+		if (t != null && t.length > 0)
+			t[0].type(b);
+		else
+			// b.append(Ason.class.getName());
+			b.append(t.getClass().getComponentType().getName());
+
+		b.append("],\n data: [");
+
+		Stream<T> json = Arrays.stream(t);
+		if (t != null && t.length > 0) {
+			T first = t[0];
+			b.append(first.json());
+
+			json.skip(1).forEach(e -> {
+				b.append(", ").append(e.json());
+			});
+		}
+		return b.append("]}").toString();
+
+	}*/
+	
+	public static <T extends Ason> String json (T[] t) {
+		StringBuilder b = new StringBuilder();
+		b.append("{type: [");
+
+		b.append(t.getClass().getComponentType().getName());
+
+		b.append("],\n types: [");
+
+		HashMap<String, Object> types = new HashMap<String, Object>();
+		
+//		b.append(types.entrySet().stream()
+//			.map(e -> e.toString())
+//			.collect(Collectors.joining(", ", "[", "]")));
+
+		types.entrySet().stream()
+			.forEach(e -> {
+				Class<?> cls = e.getClass();
+				String cn = cls.getName();
+				if (!types.containsKey(cn)) {
+					if (Ason.class.isAssignableFrom(cls)) {
+						// unknown / user type
+					}
+					else if (false)
+						;
+				}
+//					e.type2(b);
+			});
+		
+		if (types.size() > 0) {
+			boolean isfirst = true;
+			for (String tp : types.keySet()) {
+				if (!isfirst)
+					b.append(", ");
+				Object typ = types.get(tp);
+				if (typ instanceof Ason)
+					((Ason)typ).type2(b);
+				else
+					// shouldn't reach here
+					b.append("{").append(typ).append(":").append(tp);
+			}
+			
+		}
+
+		b.append("],\n data: [");
+
+		// data
+		Stream<T> json = Arrays.stream(t);
+		if (t != null && t.length > 0) {
+			T first = t[0];
+			b.append(first.json());
+
+			json.skip(1).forEach(e -> {
+				b.append(", ").append(e.json());
+			});
+		}
+		return b.append("]}").toString();
+
+	}
+	
+	public static <T extends Ason> String json (T t) {
+		// FIXME Performance problem: type() and json() should return stream.
+		/*
+		return Stream.of(
+				"type: \"",
+				t.type(), "\"}, data: ",
+				t.json()).collect(Collectors.joining("", "{", "}"));
+				*/
+		StringBuilder b = new StringBuilder();
+		b.append("type: ");
+		t.type(b);
+		b.append("}, data: ");
+		b.append(t.json());
+		return b.toString();
+	}
+}
--- antson/src/main/java/io/odysz/antson/Ason.java
+++ antson/src/main/java/io/odysz/antson/Ason.java
@@ -2,6 +2,7 @@ package io.odysz.antson;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,19 +18,93 @@ public class Ason {
 	}
 
 	@Override
-	public String toString() { return json(); }
+	public String toString() {
+		return json();
+	}
 	
+	public HashMap<String, Object> types(HashMap<String, Object> protos) {
+		
+		return protos;
+	}
+
+	public String type2(StringBuilder b) {
+		return null;
+	}
+
+	
+	public Ason type(StringBuilder b) {
+		// { com.foo.typ1: { "f1": Object, "f2": [String], "f3": {com.foo.typ2: {...}}}}
+		b.append("{");
+		b.append(getClass().getName());
+		b.append(": {");
+
+		Field flist[] = this.getClass().getDeclaredFields();
+		if (flist != null && flist.length > 0) {
+			appendField(b, flist[0], true);
+			Stream.of(flist).skip(1).forEach(f -> {
+				appendField(b, f, false);
+			});
+			b.append("}");
+		}
+		return this;
+	}
+	
+	private StringBuilder appendField(StringBuilder b, Field f, boolean isFirst) {
+		
+		if (f.getName().startsWith("this$"))
+			return b;
+
+		if (!isFirst) b.append(", ");
+
+		b.append("\"")
+		 .append(f.getName())
+		 .append("\": ");
+		
+		// [String] | {com.foo.typ2: {...}} | Object
+		Class<?> tp = f.getType();
+		boolean isArray = false;
+		if (tp.isArray()) {
+			isArray = true;
+			b.append("[");
+			tp = tp.getComponentType();
+		}
+		if (Ason.class.isAssignableFrom(tp))
+			// Ason | {com.foo.typ3: {...}}
+			try {
+				Ason obj;
+				if (isArray) {
+					Ason[] arr = (Ason[])f.get(this);
+					if (arr == null || arr.length == 0)
+						obj = null;
+					else
+						obj = arr[0];
+				}
+				else
+					obj = (Ason)f.get(this);
+
+				if (obj == null)
+					b.append(tp.getName());
+				else
+					obj.type(b);
+			}
+			catch (IllegalArgumentException | IllegalAccessException e) { e.printStackTrace(); }
+		else
+			b.append(tp.getName().replaceFirst("^java\\.lang\\.", "").replaceFirst("java.\\.util\\.", ""));
+		if (isArray)
+			b.append("]");
+		return b;
+	}
+
 	public String json() {
 		Field flist[] = this.getClass().getDeclaredFields();
 		return Stream.of(flist)
+			.filter(m -> !m.getName().startsWith("this$"))
 			.map(m -> {
 				try {
 					Class<?> t = m.getType();
 					if (t.isPrimitive())
-//						return String.valueOf(t);
 						return String.format(fmt.pairPrmv, m.getName(), t);
 					else if (t == String.class)
-						// return (String)m.get(this);
 						return String.format(fmt.pairPrmv, m.getName(), m.get(this));
 					else if (t.isArray()) {
 						return String.format(fmt.pairPrmv, m.getName(), 
@@ -44,14 +119,23 @@ public class Ason {
 										}
 							}).collect(Collectors.joining(", ", "[", "]")));
 					}
-					// else if (t.isAssignableFrom(Ason.class))
-					//	return String.format(fmt.pairPrmv, m.getName(), ((Ason)m.get(this)).json()); 
-					else if (Ason.class.isAssignableFrom(m.getDeclaringClass()))
-						return String.format(fmt.pairPrmv, m.getName(), this.json()); 
+					else if (Ason.class.isAssignableFrom(m.getDeclaringClass())) {
+						if (!m.getName().startsWith("this$"))
+							return String.format(fmt.pairPrmv, m.getName(), ((Ason)m.get(this)).json());
+//							return String.format(fmt.pairPrmv, m.getName());
+						else return "";
+//						if (!m.getName().equals("this$0"))
+//							 return String.format(fmt.pairPrmv, m.getName(), this.jvalue()); 
+////							return Stream.concat( Stream.of( m.getName(), ": "), this.json())); 
+//						else return "";
+
+//						return "";
+					}
 					else return m.get(this).toString();
 				} catch (IllegalArgumentException | IllegalAccessException e) {
 					return String.format("EXCEPTION: \"%s\"", e.getMessage());
 				}
 			}).collect(Collectors.joining(", ", "{", "}"));
 	}
+
 }
