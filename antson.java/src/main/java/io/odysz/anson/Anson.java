374d7a7e49f79d0d5a133b708b095a8f98bf7457
odysz <odysseusj@163.com>
2019-07-28 23:28:56 +0800

    0

--- antson.java/src/main/java/io/odysz/anson/Anson.java
+++ antson.java/src/main/java/io/odysz/anson/Anson.java
@@ -105,7 +105,7 @@ public class Anson {
 	protected Anson fromBlock(InputStream stream) throws IOException {
 		byte[] buf = new byte[bufLength];
 		int len = stream.read(buf);
-		while (len > 0) {
+		while (len != -1) {
 			
 		}
 
