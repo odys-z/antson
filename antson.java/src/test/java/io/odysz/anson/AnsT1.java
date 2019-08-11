a7eb7b74b45387dcd5d1ddffb296af4626d65da8
odysz <odysseusj@163.com>
2019-08-11 23:53:43 +0800

    0

---
+++ antson.java/src/test/java/io/odysz/anson/AnsT1.java
@@ -0,0 +1,17 @@
+package io.odysz.anson;
+
+public class AnsT1 extends Anson {
+
+	public class AnsM1 extends Anson {
+		String name;
+		
+		public AnsM1() {
+			name = "m1 : Anson";
+		}
+	}
+
+	protected String ver;
+	
+	public AnsM1 m;
+
+}
