896e1c7f604b8d7579ae3b3b402b400d84bbf0e3
Odys Zhou <odys.zhou@gmail.com>
2021-04-27 19:40:50 +0800

    add csharp project

---
+++ antson.csharp/antson.csharp/src/io.odysz/Anson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.odysz.anson
+{
+    public class Anson
+    {
+        public Anson() { }
+
+        public Anson ToBlock(MemoryStream outstream, JsonOpt opt = null)
+        {
+
+            return null;
+        }
+    }
+}
