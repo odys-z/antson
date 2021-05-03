e249cea402fda1b46d0c3e7f53546d40c82a4683
Odys Zhou <odys.zhou@gmail.com>
2021-05-03 23:06:06 +0800

    published 0.9.3.1

--- antson.csharp/antson.csharp/Properties/AssemblyInfo.cs
+++ antson.csharp/antson.csharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("antson.csharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# client of Antson.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("antson.csharp")]
