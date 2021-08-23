ad2a52723dfa23b625f168dcf850f79537978a80
odys zhou <odysseusj@163.com>
2021-08-23 20:28:27 +0800

    v0.9.7

--- antson.java/src/test/java/io/odysz/semantic/jprotocol/test/AnsonMsg.java
+++ antson.java/src/test/java/io/odysz/semantic/jprotocol/test/AnsonMsg.java
@@ -37,7 +37,9 @@ public class AnsonMsg <T extends AnsonBody> extends Anson {
 						stree("s-tree.serv11"),
 						/** dataset extensions<br>
 						 * @see {@link io.odysz.semantic.ext.Dataset}. */
-						dataset("ds.serv11");
+						dataset("ds.serv11"),
+						/**@deprecated For test only */
+						quiz("quiz.serv");
 		
 		private String url;
 		@Override public String url() { return url; }
