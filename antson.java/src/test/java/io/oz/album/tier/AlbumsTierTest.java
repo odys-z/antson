817c7d437141809b8b31b23f3b7f8fc4a9c01cae
Odys Zhou <odysseusj@163.com>
2022-02-03 23:50:39 -0500

    .

---
+++ antson.java/src/test/java/io/oz/album/tier/AlbumsTierTest.java
@@ -0,0 +1,14 @@
+package io.oz.album.tier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlbumsTierTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
