a5b000bb98df9211e18c05500ed0e9185c9ce742
Odys Zhou <odysseusj@163.com>
2022-02-19 18:25:31 -0500

    add AnsonFiled.shortoString default false, for shorten string printing

--- antson.java/src/main/java/io/odysz/anson/Anson.java
+++ antson.java/src/main/java/io/odysz/anson/Anson.java
@@ -38,7 +38,7 @@ public class Anson implements IJsonable {
 	public String toString() {
 		try {
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			toBlock(bos);
+			toBlock(bos, new JsonOpt().shortenOnAnnoteRequired(true));
 			return bos.toString(StandardCharsets.UTF_8.name());
 		} catch (AnsonException | IOException e) {
 			e.printStackTrace();
@@ -98,18 +98,24 @@ public class Anson implements IJsonable {
 			}
 
 			try {
-				if (!f.getType().isPrimitive()) {
-					Object v = f.get(this);
-					Class<? extends Object> vclz = v == null ? null : v.getClass();
+				Object v = f.get(this);
+				AnsonField anno = f.getAnnotation(AnsonField.class);
+				if (v != null && anno != null && anno.shortoString()
+					&& opts != null && opts.length > 0 && opts[0] != null && opts[0].shortenOnAnnotation())
+					stream.write(("\"... \"").getBytes(StandardCharsets.UTF_8));
+				else {
+					if (!f.getType().isPrimitive()) {
+						Class<? extends Object> vclz = v == null ? null : v.getClass();
 
-					writeNonPrimitive(stream, vclz, v, opts);
+						writeNonPrimitive(stream, vclz, v, opts);
+					}
+					if (f.getType() == char.class) {
+						char c = f.getChar(this);
+						stream.write(c == 0 ? '0' : c);
+					}
+					else if (f.getType().isPrimitive())
+						stream.write(String.valueOf(f.get(this)).getBytes(StandardCharsets.UTF_8));
 				}
-				if (f.getType() == char.class) {
-					char c = f.getChar(this);
-					stream.write(c == 0 ? '0' : c);
-				}
-				else if (f.getType().isPrimitive())
-					stream.write(String.valueOf(f.get(this)).getBytes(StandardCharsets.UTF_8));
 			} catch (IllegalArgumentException | IllegalAccessException e1) {
 				throw new AnsonException(0, e1.getMessage());
 			}
--- antson.java/src/main/java/io/odysz/anson/JsonOpt.java
+++ antson.java/src/main/java/io/odysz/anson/JsonOpt.java
@@ -6,6 +6,7 @@ public class JsonOpt extends Anson {
 	/**Must serialize key with quotes */
 	private boolean quotKey = true;
 
+
 	/**Must serialize key with quotes, default true */
 	public boolean quotKey() {
 		return quotKey;
@@ -13,6 +14,18 @@ public class JsonOpt extends Anson {
 
 	public JsonOpt quotKey(boolean has) {
 		quotKey = has;
+		return this;
+	}
+
+	private boolean shortenOnAnnotation;
+	public boolean shortenOnAnnotation() { return shortenOnAnnotation; }
+	
+	/**Tell Anson shorten printing a string value if required with type annotation, both true. 
+	 * @param yes
+	 * @return this
+	 */
+	public JsonOpt shortenOnAnnoteRequired(boolean yes) {
+		this.shortenOnAnnotation = yes;
 		return this;
 	}
 }
