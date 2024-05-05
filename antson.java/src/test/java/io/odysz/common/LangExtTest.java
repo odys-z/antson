64be70445b0c912859394203a114442df21343cb
odysz <odysseusj@163.com>
2024-05-05 21:05:19 +0800

    .

--- antson.java/src/test/java/io/odysz/common/LangExtTest.java
+++ antson.java/src/test/java/io/odysz/common/LangExtTest.java
@@ -1,11 +1,26 @@
 package io.odysz.common;
 
-import static io.odysz.common.LangExt.*;
+import static io.odysz.common.LangExt.endWith;
+import static io.odysz.common.LangExt.eq;
+import static io.odysz.common.LangExt.eqs;
+import static io.odysz.common.LangExt.filesize;
+import static io.odysz.common.LangExt.gt;
+import static io.odysz.common.LangExt.imagesize;
+import static io.odysz.common.LangExt.indexOf;
+import static io.odysz.common.LangExt.is;
+import static io.odysz.common.LangExt.isNull;
+import static io.odysz.common.LangExt.isblank;
+import static io.odysz.common.LangExt.ix;
+import static io.odysz.common.LangExt.join;
+import static io.odysz.common.LangExt.joinEsc;
+import static io.odysz.common.LangExt.lt;
+import static io.odysz.common.LangExt.startsOneOf;
+import static io.odysz.common.LangExt.str;
+import static io.odysz.common.LangExt.trim;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.lang.reflect.Array;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
