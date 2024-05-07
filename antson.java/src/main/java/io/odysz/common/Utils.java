4d91ce58662401f3c2e248a9e256b7c3a3602186
Ody Z <odys.zhou@gmail.com>
2024-05-07 03:24:08 -0400

    Update Utils.java

--- antson.java/src/main/java/io/odysz/common/Utils.java
+++ antson.java/src/main/java/io/odysz/common/Utils.java
@@ -316,7 +316,51 @@ public class Utils {
 				   "+", len - 1));
 	}
 
+	/**
+     * Returns a string whose value is the concatenation of this
+     * string repeated {@code count} times.
+     * <p>
+     * If this string is empty or count is zero then the empty
+     * string is returned.
+     *
+     * @param   count number of times to repeat
+     *
+     * @return  A string composed of this string repeated
+     *          {@code count} times or the empty string if this
+     *          string is empty or count is zero
+     *
+     * @throws  IllegalArgumentException if the {@code count} is
+     *          negative.
+     *
+     * @since 11
+     */
 	public static String repeat(String object, int repeat) {
-		return null;
-	}
+	        if (count < 0) {
+	            throw new IllegalArgumentException("count is negative: " + count);
+	        }
+	        if (count == 1) {
+	            return this;
+	        }
+	        final int len = value.length;
+	        if (len == 0 || count == 0) {
+	            return "";
+	        }
+	        if (Integer.MAX_VALUE / count < len) {
+	            throw new OutOfMemoryError("Required length exceeds implementation limit");
+	        }
+	        if (len == 1) {
+	            final byte[] single = new byte[count];
+	            Arrays.fill(single, value[0]);
+	            return new String(single, coder);
+	        }
+	        final int limit = len * count;
+	        final byte[] multiple = new byte[limit];
+	        System.arraycopy(value, 0, multiple, 0, len);
+	        int copied = len;
+	        for (; copied < limit - copied; copied <<= 1) {
+	            System.arraycopy(multiple, 0, multiple, copied, copied);
+	        }
+	        System.arraycopy(multiple, 0, multiple, copied, limit - copied);
+	        return new String(multiple, coder);
+	    }
 }
