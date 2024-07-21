439cc359257b96b47207e347f0ea827fc08fbd0d
Ody Zelenskyy <odys.zhou@gmail.com>
2024-07-21 18:07:45 +0800

    9.77

--- antson.java/src/main/java/io/odysz/common/LangExt.java
+++ antson.java/src/main/java/io/odysz/common/LangExt.java
@@ -12,6 +12,76 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LangExt {
+	////////// org.apache.commons.lang3 //////////////////////////////
+	// License Apache-2.0 license
+	//
+	/*
+	 * package org.apache.commons.lang3;
+	 * 
+	 * Licensed to the Apache Software Foundation (ASF) under one or more
+	 * contributor license agreements.  See the NOTICE file distributed with
+	 * this work for additional information regarding copyright ownership.
+	 * The ASF licenses this file to You under the Apache License, Version 2.0
+	 * (the "License"); you may not use this file except in compliance with
+	 * the License.  You may obtain a copy of the License at
+	 *
+	 *      http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 * Unless required by applicable law or agreed to in writing, software
+	 * distributed under the License is distributed on an "AS IS" BASIS,
+	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 * See the License for the specific language governing permissions and
+	 * limitations under the License.
+	 */
+	
+    /**
+     * Maps primitive {@link Class}es to their corresponding wrapper {@link Class}.
+     */
+    private static final Map<Class<?>, Class<?>> primitiveWrapperMap = new HashMap<>();
+
+    static {
+        primitiveWrapperMap.put(Boolean.TYPE, Boolean.class);
+        primitiveWrapperMap.put(Byte.TYPE, Byte.class);
+        primitiveWrapperMap.put(Character.TYPE, Character.class);
+        primitiveWrapperMap.put(Short.TYPE, Short.class);
+        primitiveWrapperMap.put(Integer.TYPE, Integer.class);
+        primitiveWrapperMap.put(Long.TYPE, Long.class);
+        primitiveWrapperMap.put(Double.TYPE, Double.class);
+        primitiveWrapperMap.put(Float.TYPE, Float.class);
+        primitiveWrapperMap.put(Void.TYPE, Void.TYPE);
+    }
+
+    /**
+     * Maps wrapper {@link Class}es to their corresponding primitive types.
+     */
+    private static final Map<Class<?>, Class<?>> wrapperPrimitiveMap = new HashMap<>();
+
+    static {
+        primitiveWrapperMap.forEach((primitiveClass, wrapperClass) -> {
+            if (!primitiveClass.equals(wrapperClass)) {
+                wrapperPrimitiveMap.put(wrapperClass, primitiveClass);
+            }
+        });
+    }
+    
+    /**
+     * Returns whether the given {@code type} is a primitive wrapper ({@link Boolean}, {@link Byte}, {@link Character},
+     * {@link Short}, {@link Integer}, {@link Long}, {@link Double}, {@link Float}).
+     *
+     * @param type The class to query or null.
+     * @return true if the given {@code type} is a primitive wrapper ({@link Boolean}, {@link Byte}, {@link Character},
+     *         {@link Short}, {@link Integer}, {@link Long}, {@link Double}, {@link Float}).
+     * @since 3.1
+     */
+    public static boolean isPrimitive(final Class<?> type) {
+        return wrapperPrimitiveMap.containsKey(type);
+    }
+	////////// org.apache.commons.lang3 //////////////////////////////
+
+    public static boolean isPrimitive(Object target) {
+    	return target != null && isPrimitive(target.getClass()); 
+    }
+    
 	/**
 	 * Split and trim elements.
 	 * <p>Empty element won't be ignored if there are 2 consequent separator. <br>
